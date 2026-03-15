5c8db7a0b8e46fc457a5de1dba5cf05e7df4b5a2
Deepika Konyala <deepikakonyala30@gmail.com>
2026-03-15 19:48:12 +0530

    Initial commit - Mini C compiler using Lex and Yacc

---
+++ input1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+    int a;
+    int b;
+    a = 5;
+    b = a + 10;
+    return 0;
+}
---
+++ input2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+    int i=1;
+    float f = 2.5;
+    char c = 'A';
+    int x = 3;
+    i = x + 5;
+    return 3;
+}
